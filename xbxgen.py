96172c5a40703c54f348765db4c8198a944374d2
ezwayx <142753405+ezwayx@users.noreply.github.com>
2023-08-22 05:20:59 -0400

    Update xbxgen.py

--- xbxgen.py
+++ xbxgen.py
@@ -1,125 +1,110 @@
 import random
-import time
+import os
+from datetime import datetime
 
-xbox_usernames = [
-    "SharpKnives", "TheZodiac", "OsamasGhost", "Angelofdeath", "DeathSquad",
-    "Veteranofdeath", "T3rr0r1st", "Ebola", "LookURDead", "SharpKnives",
-    "SepticMoma", "AssasinMeetUp", "TWitMeet", "AssasinFaceOff", "WickedImpulse",
-    "B4UShout", "UrAssDontLie", "LazyKiller", "FireKiller", "Damnhunter", "Mahakaal"
-]
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-pass_durations = [
-    "1 Month Game Pass", "3 Month Game Pass",
-    "6 Month Game Pass", "12 Month Game Pass"
-]
+def generate_xbox_gift_card():
+    code = ''.join(str(random.randint(1, 9)) for _ in range(16))
+    balance = random.randint(20, 100)
+    generated_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return f"Xbox Gift Card Code: {code}\nBalance: ${balance}\nGenerated: {generated_time}"
 
-def generate_random_username():
-    return random.choice(xbox_usernames)
-
-def generate_random_code():
-    return ''.join(str(random.randint(0, 9)) for _ in range(16))
-
-def generate_random_balance():
-    return random.randint(10, 100)
-
-def generate_random_duration():
-    return random.choice(pass_durations)
-
-def create_gift_card():
-    username = generate_random_username()
-    code = generate_random_code()
-    balance = generate_random_balance()
-    timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
-
-    gift_card = f"""
-xʙᴏx ᴄᴀʀᴅ ɢᴇɴ
-╔═════════════════╗
-║  XBOX GIFT CARD ║
-╠═════════════════╣
-║ Username: {username}
-║ Code: {code}
-║ Balance: ${balance}
-║ Last Updated: {timestamp}
-╚═════════════════╝
-"""
-
-    return gift_card
-
-def create_game_pass_code():
-    code = generate_random_code()
-    duration = generate_random_duration()
-    timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
-
-    game_pass_code = f"""
-xʙᴏx ᴄᴀʀᴅ ɢᴇɴ
-╔═══════════════════╗
-║ = CREATE GAME PASS║
-║       CODE =      ║
-╠═══════════════════╣
-║ Code: {code}
-║ Duration: {duration}
-║ Last Updated: {timestamp}
-╚═══════════════════╝
-"""
-
-    return game_pass_code
-
-def current_update():
-    return """
-xʙᴏx ᴄᴀʀᴅ ɢᴇɴ
-┏━━━━━━━━━━━━━━━━━━━┓
-┃= Check github for ┃
-┃updates =          ┃
-┃                   ┃
-┗━━━━━━━━━━━━━━━━━━━┛
-"""
+def generate_xbox_gamepass():
+    code = ''.join(str(random.randint(1, 9)) for _ in range(16))
+    durations = ["1 month", "3 months", "1 year"]
+    duration = random.choice(durations)
+    generated_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return f"Xbox GamePass/Gold Code: {code}\nDuration: {duration}\nGenerated: {generated_time}"
 
 def main():
-    while True:
-        print("\n" + "="*30)
-        print("        DASHBOARD MENU        ")
-        print("="*30)
-        print("(1) Create Gift Card")
-        print("(2) Create Game Pass Code")
-        print("(3) Current Update")
-        print("(4) Exit")
-        choice = int(input("Enter your choice (1/2/3/4): "))
+    title = """
 
-        if choice == 1:
+ ██████╗ ██████╗ ███╗   ██╗███████╗ ██████╗ ██╗     ███████╗             
+██╔════╝██╔═══██╗████╗  ██║██╔════╝██╔═══██╗██║     ██╔════╝             
+██║     ██║   ██║██╔██╗ ██║███████╗██║   ██║██║     █████╗               
+██║     ██║   ██║██║╚██╗██║╚════██║██║   ██║██║     ██╔══╝               
+╚██████╗╚██████╔╝██║ ╚████║███████║╚██████╔╝███████╗███████╗             
+ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚══════╝╚══════╝             
+                                                                         
+ ██████╗ ██╗███████╗████████╗     ██████╗ ██████╗ ██████╗ ███████╗       
+██╔════╝ ██║██╔════╝╚══██╔══╝    ██╔════╝██╔═══██╗██╔══██╗██╔════╝       
+██║  ███╗██║█████╗     ██║       ██║     ██║   ██║██║  ██║█████╗         
+██║   ██║██║██╔══╝     ██║       ██║     ██║   ██║██║  ██║██╔══╝         
+╚██████╔╝██║██║        ██║       ╚██████╗╚██████╔╝██████╔╝███████╗       
+ ╚═════╝ ╚═╝╚═╝        ╚═╝        ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝       
+                                                                         
+███████╗ ██████╗ █████╗ ███╗   ███╗██╗    ██╗ █████╗ ██████╗ ███████╗    
+██╔════╝██╔════╝██╔══██╗████╗ ████║██║    ██║██╔══██╗██╔══██╗██╔════╝    
+███████╗██║     ███████║██╔████╔██║██║ █╗ ██║███████║██████╔╝█████╗      
+╚════██║██║     ██╔══██║██║╚██╔╝██║██║███╗██║██╔══██║██╔══██╗██╔══╝      
+███████║╚██████╗██║  ██║██║ ╚═╝ ██║╚███╔███╔╝██║  ██║██║  ██║███████╗    
+╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝     ╚═╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝    
+                                                                         
+                                                            
+                                                                                                                                  
+"""
+    while True:
+        clear_screen()
+        print(title)
+        print("Main Menu")
+        print("(1) Enter Generator")
+        print("(2) Current Update")
+        
+        choice = input("Select 1 or 2: ")
+        
+        if choice == "1":
             while True:
-                gift_card_output = create_gift_card()
-                print(gift_card_output)
-                print("\n(1) Regenerate Gift Card")
-                print("(2) Back to Dashboard")
-                back_choice = int(input("Enter your choice (1/2): "))
-                if back_choice == 1:
-                    continue
-                elif back_choice == 2:
+                clear_screen()
+                print(title)
+                print("\n(1) Xbox Codes")
+                print("(2) PS4 Codes")
+                print("(3) Back")
+                
+                generator_choice = input("Select an option: ")
+                
+                if generator_choice == "1":
+                    while True:
+                        clear_screen()
+                        print(title)
+                        print("\n(1) Xbox Gift Card Code")
+                        print("(2) Xbox GamePass/Gold")
+                        print("(3) Back")
+                        
+                        xbox_choice = input("Select an option: ")
+                        
+                        if xbox_choice == "1":
+                            xbox_gift_card = generate_xbox_gift_card()
+                            clear_screen()
+                            print(title)
+                            print("\n" + xbox_gift_card)
+                            input("Press Enter to continue...")
+                        elif xbox_choice == "2":
+                            xbox_gamepass = generate_xbox_gamepass()
+                            clear_screen()
+                            print(title)
+                            print("\n" + xbox_gamepass)
+                            input("Press Enter to continue...")
+                        elif xbox_choice == "3":
+                            break
+                elif generator_choice == "2":
+                    clear_screen()
+                    print(title)
+                    print("\nComing soon")
+                    input("Press Enter to continue...")
+                elif generator_choice == "3":
                     break
-        elif choice == 2:
-            while True:
-                game_pass_code_output = create_game_pass_code()
-                print(game_pass_code_output)
-                print("\n(1) Regenerate Game Pass Code")
-                print("(2) Back to Dashboard")
-                back_choice = int(input("Enter your choice (1/2): "))
-                if back_choice == 1:
-                    continue
-                elif back_choice == 2:
-                    break
-        elif choice == 3:
-            while True:
-                update_message = current_update()
-                print(update_message)
-                print("\n(1) Back to Dashboard")
-                back_choice = int(input("Enter your choice (1): "))
-                if back_choice == 1:
-                    break
-        elif choice == 4:
-            print("Exiting the dashboard. Goodbye!")
-            break
+        elif choice == "2":
+            clear_screen()
+            print(title)
+            print("\nComing soon")
+            input("Press Enter to continue...")
         else:
-            print("Invalid choice. Please select 1, 2, 3, or 4.")
+            clear_screen()
+            print(title)
+            print("\nInvalid choice. Please select 1 or 2.")
+            input("Press Enter to continue...")
 
 if __name__ == "__main__":
     main()
